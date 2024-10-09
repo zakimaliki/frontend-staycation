eb5a7e9948a8461693aea7cfd39778731273c6b7
malikizaki94 <malikizakitop@gmail.com>
2024-10-09 11:47:55 +0700

    add proptype

--- src/elements/Button/index.jsx
+++ src/elements/Button/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import propTypes from 'prop-types'
 
-const Button = () => {
+const Button = (props) => {
   return (
     <div>Button</div>
   )
+}
+
+Button.propTypes ={
+    type : propTypes.oneOf(["Button", "link"]),
+    onclick : propTypes.func,
+    target : propTypes.string,
+    className : propTypes.string,
+    isDisabled : propTypes.bool,
+    isLoading : propTypes.bool,
+    isSmall : propTypes.bool,
+    isLarge : propTypes.bool,
+    isBlock : propTypes.bool,
+    hasShadow: propTypes.bool
 }
 
 export default Button
