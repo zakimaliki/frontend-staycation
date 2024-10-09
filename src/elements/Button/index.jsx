11f10bf40e21856fe6614d485474158fe046726c
malikizaki94 <malikizakitop@gmail.com>
2024-10-09 12:11:35 +0700

    add proptype

--- src/elements/Button/index.jsx
+++ src/elements/Button/index.jsx
@@ -1,23 +1,53 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import propTypes from 'prop-types'
 
 const Button = (props) => {
-  return (
-    <div>Button</div>
-  )
+    const className = [props.className]
+    if (propTypes.isPrimary) className.push("btn-primary")
+    if (propTypes.isLarge) className.push("btn-lg")
+    if (propTypes.isSmall) className.push("btn-sm")
+    if (propTypes.isBlock) className.push("btn-block")
+    if (propTypes.hasShadow) className.push("btn-shadow")
+
+    const onClick = () => {
+        if (props.onClick) props.onClick()
+    }
+
+    if (props.type === "link") {
+        if (props.isExternal) {
+            return (
+                <a href={props.href} className={className.join(" ")} style={props.style} target={props.target === "_blank" ? "_blank" : undefined} rel={props.target === "_blank" ? "noopener noreferrer" : undefined}>
+                    {props.children}
+                </a>
+            )
+        } else {
+            <Link to={props.href}
+                className={className.join("")}
+                style={props.style}
+                onClick={onClick}
+            >
+                {props.children}
+            </Link>
+        }
+    }
+
+    return (
+        <div>Button</div>
+    )
 }
 
-Button.propTypes ={
-    type : propTypes.oneOf(["Button", "link"]),
-    onclick : propTypes.func,
-    target : propTypes.string,
-    className : propTypes.string,
-    isDisabled : propTypes.bool,
-    isLoading : propTypes.bool,
-    isSmall : propTypes.bool,
-    isLarge : propTypes.bool,
-    isBlock : propTypes.bool,
+Button.propTypes = {
+    type: propTypes.oneOf(["Button", "link"]),
+    onClick: propTypes.func,
+    href: propTypes.string,
+    target: propTypes.string,
+    className: propTypes.string,
+    isDisabled: propTypes.bool,
+    isLoading: propTypes.bool,
+    isSmall: propTypes.bool,
+    isLarge: propTypes.bool,
+    isBlock: propTypes.bool,
     hasShadow: propTypes.bool
 }
 
