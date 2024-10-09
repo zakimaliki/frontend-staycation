4130985ff373907f05849d85ff788545f1092d0d
malikizaki94 <malikizakitop@gmail.com>
2024-10-09 12:25:31 +0700

    button component

--- src/elements/Button/index.jsx
+++ src/elements/Button/index.jsx
@@ -14,6 +14,22 @@ const Button = (props) => {
         if (props.onClick) props.onClick()
     }
 
+    if (props.isDisabled || props.isLoading) {
+        if (props.isDisabled) className.push("disabled")
+        return (
+            <span className={className.join("")} style={props.style}>
+                {props.isLoading ? (
+                    <>
+                        <span className="spinner-border spinner-border-sm mx-5"></span>
+                        <span className="sr-only">Loading...</span>
+                    </>
+                ) : (props.children
+                )}
+            </span>
+        )
+    }
+
+
     if (props.type === "link") {
         if (props.isExternal) {
             return (
@@ -22,18 +38,22 @@ const Button = (props) => {
                 </a>
             )
         } else {
-            <Link to={props.href}
-                className={className.join("")}
-                style={props.style}
-                onClick={onClick}
-            >
-                {props.children}
-            </Link>
+            return (
+                <Link to={props.href}
+                    className={className.join("")}
+                    style={props.style}
+                    onClick={onClick}
+                >
+                    {props.children}
+                </Link>
+            )
         }
     }
 
     return (
-        <div>Button</div>
+        <button className={className.join("")}
+            style={props.style}
+            onClick={onClick}>{props.children}</button>
     )
 }
 
